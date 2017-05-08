9a7fb7e3ccc07e13b3817bed67b3a82b44d6fc73
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-08 14:26:44 +0900

    Install capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+# config valid only for current version of Capistrano
+lock "3.8.1"
+
+set :application, "my_app_name"
+set :repo_url, "git@example.com:me/my_repo.git"
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/keypair.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
