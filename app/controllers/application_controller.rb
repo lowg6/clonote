299c70c4ec5d65b929325427e792d55c175389df
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-10 18:59:55 +0900

    Add after_sign_out_path_for method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
