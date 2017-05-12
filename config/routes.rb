4344c3ca3eee47b1f23b6061051631f1c38cdc82
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-12 16:57:42 +0900

    Add routings related to following

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'notes#index'
   resources :notes
-  resources :users, only: :show
+  resources :users, only: :show do
+    member do
+     get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
 end
