094f32db3b5fb796e96d88c8827762a9672fb053
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-14 02:49:07 +0900

    Add default image url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170513090840) do
+ActiveRecord::Schema.define(version: 20170513171320) do
 
   create_table "notes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
@@ -30,22 +30,22 @@ ActiveRecord::Schema.define(version: 20170513090840) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                                default: "", null: false
-    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "email",                                default: "",                                                                        null: false
+    t.string   "encrypted_password",                   default: "",                                                                        null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                        default: 0,  null: false
+    t.integer  "sign_in_count",                        default: 0,                                                                         null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
+    t.datetime "created_at",                                                                                                               null: false
+    t.datetime "updated_at",                                                                                                               null: false
     t.string   "nickname"
     t.text     "description",            limit: 65535
     t.string   "header_image"
-    t.string   "image"
+    t.string   "image",                                default: "https://s3-ap-northeast-1.amazonaws.com/mxi/uploads/default_profile.png"
     t.string   "noteid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
