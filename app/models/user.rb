76f183a68531a4925e2bc16c41c66b48e7fb8938
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-13 18:26:49 +0900

    Add validation of noteid with uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :email, presence: true, uniqueness: true
+  validates :noteid, uniqueness: true
   has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
   has_many :followings, through: :following_relationships
   has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
