f0d66c79355f810c9d41b424e1fbca7ef85301aa
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-13 18:26:05 +0900

    Change uses users id to noteid

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'notes#index'
   resources :notes
-  resources :users, only: :show do
+  resources :users, param: :noteid, path: '/', only: [:show, :edit] do
     member do
      get :following, :followers
     end
