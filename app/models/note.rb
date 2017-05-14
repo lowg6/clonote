9622e09dc2ad67bc405b093313de4824d08e57d7
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-15 03:34:02 +0900

    Create an association

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,5 @@
 class Note < ApplicationRecord
   validates :title, presence: true
   validates :price, presence: true
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :followings, through: :following_relationships
   has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
   has_many :followers, through: :follower_relationships
+  has_many :notes
   mount_uploader :header_image, ImageUploader
   mount_uploader :image, ImageUploader
 
