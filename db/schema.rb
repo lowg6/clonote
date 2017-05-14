25de5cad5bf5612ff784a15343ac7f1588858302
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-14 21:40:39 +0900

    Add user_id column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170514095739) do
+ActiveRecord::Schema.define(version: 20170514124021) do
 
   create_table "notes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170514095739) do
     t.datetime "updated_at",                             null: false
     t.string   "header_image"
     t.integer  "price",                      default: 0
+    t.integer  "user_id"
   end
 
   create_table "relationships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
