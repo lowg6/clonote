68575155aaaa6b66036f1cbeffe4d919791ec2ed
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-15 03:37:31 +0900

    Add methods

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,20 @@ class Note < ApplicationRecord
   validates :title, presence: true
   validates :price, presence: true
   belongs_to :user
+
+  def last(limit)
+    user.notes.order('created_at DESC').limit(limit)
+  end
+
+  def prev
+    user.notes.where('id < ?', self.id).order('id DESC').first
+  end
+ 
+  def next
+    user.notes.where('id > ?', self.id).order('id ASC').first
+  end
+
+  def created_at_jp
+    self.created_at.strftime("%Y/%m/%d(#{%w(日 月 火 水 木 金 土)[self.created_at.wday]}) %X")
+  end
 end
