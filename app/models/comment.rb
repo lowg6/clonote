98fc0b1cf957873c60108aeff53ffdb5cca3e79b
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-15 20:36:24 +0900

    Add japanese datetime displayings of comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,8 @@ class Comment < ApplicationRecord
   validates :note_id, presence: true
   belongs_to :note
   belongs_to :user
+
+  def created_at_jp
+    self.created_at.strftime("%Y/%m/%d(#{%w(日 月 火 水 木 金 土)[self.created_at.wday]}) %X")
+  end
 end
