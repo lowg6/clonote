8a4914d64146cfccbf1a1cb3abde7e3bd257433d
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-15 20:35:53 +0900

    Add associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   validates :text, presence: true
   validates :user_id, presence: true
   validates :note_id, presence: true
+  belongs_to :note
+  belongs_to :user
 end
