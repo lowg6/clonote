b42a99be980f3274d3ac2bc30143d6974e21603b
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-16 00:42:26 +0900

    Add routings

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root 'notes#index'
 
-  resources :notes
   get '/drafts', to: 'notes#index_draft'
 
+  resources :notes
   resources :comments, only: :create
-  
+  resources :favorites, only: [:create, :destroy]
   resources :users, param: :noteid, path: '/' do
     member do
-     get :following, :followers
+     get :following, :followers, :favorite
     end
   end
   resources :relationships, only: [:create, :destroy]
