a315134007664de04ce47564cb1bf99f26e78a7e
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-16 07:13:02 +0900

    Create magazine model

---
+++ db/migrate/20170515220755_create_magazines.rb
@@ -0,0 +1,12 @@
+class CreateMagazines < ActiveRecord::Migration[5.0]
+  def change
+    create_table :magazines do |t|
+      t.string :title
+      t.text :description
+      t.integer :price, default: 0
+      t.boolean :is_public, default: false, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515174645) do
+ActiveRecord::Schema.define(version: 20170515220755) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "text",       limit: 65535
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20170515174645) do
     t.index ["note_id"], name: "index_favorites_on_note_id", using: :btree
     t.index ["user_id", "note_id"], name: "index_favorites_on_user_id_and_note_id", unique: true, using: :btree
     t.index ["user_id"], name: "index_favorites_on_user_id", using: :btree
+  end
+
+  create_table "magazines", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title"
+    t.text     "description", limit: 65535
+    t.integer  "price",                     default: 0
+    t.boolean  "is_public",                 default: false, null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
   end
 
   create_table "notes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
