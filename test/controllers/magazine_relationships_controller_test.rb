575ee93e6e6ff978a37d4d0893da892db4719101
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-17 05:03:20 +0900

    Create magazine relationships controller

---
+++ test/controllers/magazine_relationships_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MagazineRelationshipsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/magazine_relationship_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MagazineRelationshipTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
