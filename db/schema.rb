c7ad68f7083ec08540edd1af101323d487c2e8a4
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-16 21:31:02 +0900

    Add user id as integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516072647) do
+ActiveRecord::Schema.define(version: 20170516115256) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "text",       limit: 65535
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170516072647) do
     t.boolean  "is_public",                  default: false, null: false
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
+    t.integer  "user_id"
   end
 
   create_table "notes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
