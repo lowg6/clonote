7009e0d7d6885e8b4282b952ed2ad02f8307550f
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-17 08:15:42 +0900

    Add users notes into show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: :show
 
   def show
+    @notes = @user.notes.order('created_at DESC')
   end
 
   def edit
