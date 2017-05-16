5f1ab634a8c2a83f5a1e8bae02f1198e3114de09
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-17 06:01:05 +0900

    Add validations

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,4 +1,8 @@
 class Magazine < ApplicationRecord
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :is_public, presence: true
+  validates :user_id, presence: true
   belongs_to :user
   has_many :notes
   has_many :magazine_relationships, dependent: :destroy
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,8 @@
 class Note < ApplicationRecord
   validates :title, presence: true
   validates :price, presence: true
+  validates :user_id, presence: true
+  validates :is_draft, presence: true
   has_many :favorites, dependent: :destroy
   has_many :favoriting_users, through: :favorites, source: :user
   has_many :comments, dependent: :destroy
