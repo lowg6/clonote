e44cd57ecfbc40ca20b1735240fe0316903425f1
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-17 00:20:55 +0900

    Fix all magazines controller

--- app/controllers/magazines_controller.rb
+++ app/controllers/magazines_controller.rb
@@ -1,74 +1,50 @@
 class MagazinesController < ApplicationController
   before_action :set_magazine, only: [:show, :edit, :update, :destroy]
 
-  # GET /magazines
-  # GET /magazines.json
   def index
-    @magazines = Magazine.all
+    @magazines = current_user.magazines.order('created_at DESC')
   end
 
-  # GET /magazines/1
-  # GET /magazines/1.json
   def show
+    unless @magazine.is_public
+      unless view_context.current_user?(@magazine.user)
+        redirect_to root_path
+      end
+    end
+
+    @user = @magazine.user
   end
 
-  # GET /magazines/new
   def new
-    @magazine = Magazine.new
+    @magazine = current_user.magazines.new
   end
 
-  # GET /magazines/1/edit
   def edit
+    redirect_to root_path unless view_context.current_user?(@magazine.user)
   end
 
-  # POST /magazines
-  # POST /magazines.json
   def create
-    @magazine = Magazine.new(magazine_params)
-
-    respond_to do |format|
-      if @magazine.save
-        format.html { redirect_to @magazine, notice: 'Magazine was successfully created.' }
-        format.json { render :show, status: :created, location: @magazine }
-      else
-        format.html { render :new }
-        format.json { render json: @magazine.errors, status: :unprocessable_entity }
-      end
-    end
+    @magazine = current_user.magazines.create(magazine_params)
+    redirect_to @magazine
   end
 
-  # PATCH/PUT /magazines/1
-  # PATCH/PUT /magazines/1.json
   def update
-    respond_to do |format|
-      if @magazine.update(magazine_params)
-        format.html { redirect_to @magazine, notice: 'Magazine was successfully updated.' }
-        format.json { render :show, status: :ok, location: @magazine }
-      else
-        format.html { render :edit }
-        format.json { render json: @magazine.errors, status: :unprocessable_entity }
-      end
-    end
+    @magazine.update(magazine_params)
+    redirect_to @magazine
   end
 
-  # DELETE /magazines/1
-  # DELETE /magazines/1.json
   def destroy
     @magazine.destroy
-    respond_to do |format|
-      format.html { redirect_to magazines_url, notice: 'Magazine was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to magazines_url
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_magazine
-      @magazine = Magazine.find(params[:id])
-    end
+   
+  def set_magazine
+    @magazine = Magazine.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def magazine_params
-      params.fetch(:magazine, {})
-    end
+  def magazine_params
+    params.require(:magazine).permit(:title, :description, :header_image, :price, :is_public).merge(user_id: current_user.id)
+  end
 end
