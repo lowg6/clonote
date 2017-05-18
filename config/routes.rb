0b94000c3b5938079149a41176f8310843dacd70
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-19 08:43:51 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  resources :magazines
   devise_for :users
   root 'notes#index'
 
   get '/drafts', to: 'notes#index_draft'
+  get '/recommend', to: 'notes#index_recommend'
   get '/hashtag/:id', to: 'notes#index_hashtag'
+  get '/category/:id', to: 'notes#index_category'
 
-  resources :notes
+  resources :notes do
+    collection do
+      get :set_search_result_users_hashtags
+    end
+  end
+  resources :magazines
   resources :comments, only: :create
   resources :purchases, only: [:index, :new, :create]
   resources :users, param: :noteid, path: '/' do
