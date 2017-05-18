0dc6049f5f8fbccab762dec14ab59daa2df49cc9
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-18 09:47:29 +0900

    Add validations

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,6 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :note
+  validates :user_id, presence: true
+  validates :note_id, presence: true
 end
--- app/models/magazine_relationship.rb
+++ app/models/magazine_relationship.rb
@@ -1,4 +1,6 @@
 class MagazineRelationship < ApplicationRecord
   belongs_to :user
   belongs_to :magazine
+  validates :user_id, presence: true
+  validates :magazine_id, presence: true
 end
