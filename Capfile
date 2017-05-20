e0cb972259fcc6dda86d5aac318e856fe28e44fd
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-21 07:50:55 +0900

    Add some options

--- Capfile
+++ Capfile
@@ -1,10 +1,10 @@
-require "capistrano/setup"
-require "capistrano/deploy"
-
+require 'capistrano/setup'
+require 'capistrano/deploy'
+require 'capistrano/rails
 require 'capistrano/rbenv'
 require 'capistrano/bundler'
 require 'capistrano/rails/assets'
 require 'capistrano/rails/migrations'
 require 'capistrano3/unicorn'
 
-Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
+Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -3,17 +3,20 @@ lock "3.8.1"
 
 set :application, "clonote"
 set :repo_url, "git@github.com:Brainya/clonote.git"
-
+set :branch, 'master'
+set :deploy_to, '/var/www/clonote'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
+set :stage, :production
+set :rails_env, 'production'
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/keypair.pem']
 
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+# set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+# set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
