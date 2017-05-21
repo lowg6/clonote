84e1f214ddff1610a48842102a16977320e8943a
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-21 21:06:02 +0900

    Change unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,14 +1,14 @@
 app_path = File.expand_path('../../', __FILE__)
 
-worker_processes 1
-
+worker_processes 2
 working_directory app_path
+
+timeout 30
+
+listen "#{app_path}tmp/sockets/unicorn.sock"
 pid "#{app_path}/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
-
-listen 3000
-timeout 60
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
