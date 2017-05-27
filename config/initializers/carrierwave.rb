32086db0cf55b9f5428d7aaa85670f08df2756a9
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-28 08:36:59 +0900

    Remove config.asset_host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,5 +8,4 @@ CarrierWave.configure do |config|
       
   config.fog_public = true
   config.fog_directory = ENV['AWS_BUCKET_NAME']
-  # config.asset_host = "https://s3-#{ENV['AWS_REGION']}.amazonaws.com/#{ENV['AWS_BUCKET_NAME']}"
 end
