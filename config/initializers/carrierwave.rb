e4d491912e0b4d68820e28fdf02ccc1ae513aaf9
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-28 07:47:13 +0900

    Change to debug

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
-  # config.fog_credentials = {
-  #   provider: 'AWS',
-  #   aws_access_key_id: 'aaaa',
-  #   ENV['AWS_ACCESS_KEY_ID'],
-  #   aws_secret_access_key: 'aaaa',
-  #   ENV['AWS_SECRET_ACCESS_KEY'],
-  #   region: ENV['AWS_REGION']
-  # }
+  config.fog_credentials = {
+    provider: 'AWS',
+    # aws_access_key_id: 'aaaa',
+    # ENV['AWS_ACCESS_KEY_ID'],
+    # aws_secret_access_key: 'aaaa',
+    # ENV['AWS_SECRET_ACCESS_KEY'],
+    # region: ENV['AWS_REGION']
+  }
       
   config.fog_public = true
   config.fog_directory = ENV['AWS_BUCKET_NAME']
