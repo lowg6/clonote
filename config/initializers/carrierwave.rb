bb17ec1a32de0231d5cfec5bcadef1b3c30e0588
Brainya <user@Hirokis-MacBook-Pro.local>
2017-05-28 07:49:11 +0900

    Change to default

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    # aws_access_key_id: 'aaaa',
-    # ENV['AWS_ACCESS_KEY_ID'],
-    # aws_secret_access_key: 'aaaa',
-    # ENV['AWS_SECRET_ACCESS_KEY'],
-    # region: ENV['AWS_REGION']
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_REGION']
   }
       
   config.fog_public = true
